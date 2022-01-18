6d7b346c683732ddcb3349a3b8a18edc78151c20
johnny valasith <johnnyvalasith@gmail.com>
2022-01-18 22:41:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello World! Sample 02');
 }).listen(4000);
